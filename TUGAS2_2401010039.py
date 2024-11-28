f145a2d3010c0101f8ef5386ca41171c14b73843
NiKdWidyaKiranaPutri <widyakirana33@gmail.com>
2024-11-28 21:41:52 +0800

    Add files via upload

---
+++ TUGAS2_2401010039.py
@@ -0,0 +1,20 @@
+"""
+Nama  : Ni Kadek Widya Kirana Putri
+NIM   : 2401010039
+Kelas : K
+Tugas :
+Membuat algoritma berupa flowchart untuk
+konversi satuan dari meter ke cm dan implementasi ke kode python
+dengan menerapkan konsep komentar, input dan output
+"""
+
+#Konversi satuan m ke cm
+#input
+m = input("Masukkan angka : ")
+
+#proses
+hasil = int(m)*100
+txhasil = str(hasil)+ "cm"
+
+#Output
+print(txhasil)
